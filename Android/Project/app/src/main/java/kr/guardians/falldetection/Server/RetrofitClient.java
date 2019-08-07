7f9e39b4c800d7bfdeb7a4161b43ac79bc4bf3ec
haemin <goals001214@gmail.com>
2019-08-07 20:05:32 +0900

    Revert "no message"
    This reverts commit e5ff063b01f158589a69c1e5be959c6238a01980.

--- Android/Project/app/src/main/java/kr/guardians/falldetection/Server/RetrofitClient.java
+++ Android/Project/app/src/main/java/kr/guardians/falldetection/Server/RetrofitClient.java
@@ -1,25 +1,4 @@
 package kr.guardians.falldetection.Server;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class RetrofitClient {
-    private static Retrofit retrofit = null;
-
-    public static Retrofit getRetrofit() {
-
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://reqres.in/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
-
-        return retrofit;
-    }
 }
