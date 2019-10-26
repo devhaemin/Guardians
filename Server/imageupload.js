6eeaf5129819b5359ff8cb8c4a1358f2cd9ca8e2
komh0086 <50826800+komh0086@users.noreply.github.com>
2019-10-27 00:05:16 +0900

    Add Server Package

---
+++ Server/imageupload.js
@@ -0,0 +1,50 @@
+var Q = require('q');
+var mysql = require('mysql');
+var moment = require('moment');
+var fs = require('fs');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
+var multer = require('multer');
+var client = mysql.createConnection({
+	host : 'localhost',
+	user : 'ict',
+	password : 'rkeldjswm',
+	database : 'ictDB'
+});
+exports.upload = function(req, res){
+	var date = moment().format('YYYY-MM-DD HH:mm:ss');
+	console.log('imageupload post time : ', date);
+	console.log('hey : ', req.body);
+	console.log('par : ', req.query);
+	var deferred = Q.defer();
+	var deviceID = req.query.filename;
+	client.query('select patientSeq from raspberryPI where deviceID = ?', deviceID, function(error, result, field){
+		console.log(result[0].patientSeq);
+		var storage = multer.diskStorage({
+			destination: function(req, file, cb){
+				cb(null,'/root/img/');
+			},
+			filename: function(req, file, cb){
+				file.uploadFile = {
+					name : result[0].patientSeq,
+					ext : file.mimetype.split('/')[1]
+				};
+				cb(null, file.uploadFile.name + '.' + file.uploadFile.ext);
+			}
+		});
+		var upload = multer({ storage: storage }).single('file');
+		upload(req, res, function(err){
+			if (err) {
+				console.log('upload fail');
+				deferred.reject();
+				res.send();
+			}
+			else {
+				deferred.resolve(req.file.uploadedFile);
+				console.log('upload');
+				res.send();
+			}
+		});
+		return deferred.promise;
+	})
+};
